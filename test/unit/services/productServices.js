d7209820f4d6b20f6df448b7c7126553494ddd14
User <gabrielkleber006@gmail.com>
2022-04-26 18:22:20 -0300

    fix tests 4

--- test/unit/services/productServices.js
+++ test/unit/services/productServices.js
@@ -6,17 +6,17 @@ const productsServices = require('../../../services/index');
 
 describe('Product Service', () => {
 
-  describe('testa a função createSale', () => {
+  describe('testa a função createProduct', () => {
     beforeEach(() => {
-        sinon.stub(productsModels.updateProducts,'updateProducts').returns(1);
+        sinon.stub(productsModels.insertProduct, 'insertProduct').resolves([{id:1}]);
     });
     afterEach(async () => {
-      productsModels.updateProducts.updateProducts.restore();
+      productsModels.insertProduct.insertProduct.restore();
     });
 
     it('A função "createProduct deve retornar" um objeto com informações', async () => {
       const response = await productsServices.createProduct.createProduct('test', 1);
-      expect(response).to.be.an('object');
+      expect(response).to.be.deep.equals([{id:1}]);
     });
   });
 });
--- test/unit/services/salesServices.js
+++ test/unit/services/salesServices.js
@@ -5,20 +5,23 @@ const productsModels = require('../../../models/index');
 const productsServices = require('../../../services/index');
 describe('Product Service', () => {
 
-  const res = {};
-  const req = {};
-
   describe('testa a função createSale', () => {
     beforeEach(() => {
-        sinon.stub(productsModels.updateSale,'updateSale').returns(1);
+        sinon.stub(productsModels.insertSale,'insertSale').resolves(1);
     });
     afterEach(async () => {
-      productsModels.updateSale.updateSale.restore();
+      productsModels.insertSale.insertSale.restore();
     });
 
     it('A função "createSale deve retornar" um objeto com informações', async () => {
       const response = await productsServices.createSale.createSale([{ productId: 1, quantity: 1 }]);
-      expect(response).to.be.an('object');
+      expect(response).to.be.deep.equals({
+        id:1,
+        itemsSold:[{
+          productId:1,
+          quantity:1
+        }]
+      });
     });
   })
 });
